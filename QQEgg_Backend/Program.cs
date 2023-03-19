ed1f1813b260a632a55559c306f0582c59658a85
DESKTOP-38FFEEC\USER <enya19931025@gmail.com>
2023-03-19 16:34:43 +0800

    no message

--- QQEgg_Backend/Program.cs
+++ QQEgg_Backend/Program.cs
@@ -3,12 +3,17 @@ using QQEgg_Backend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the DI container.
+string dbXConnectionString = builder.Configuration.GetConnectionString("dbXConnection");
+builder.Services.AddDbContext<dbXContext>(options => { 
+    options.UseSqlServer(connectionString: dbXConnectionString);
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<dbXContext>(
- options => options.UseSqlServer(
- builder.Configuration.GetConnectionString("dbXConnection")
+options => options.UseSqlServer(
+builder.Configuration.GetConnectionString("dbXConnection")
 ));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
