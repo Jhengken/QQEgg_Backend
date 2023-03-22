0861bcae51b7561c81ad6daf91bc54b8a2ef2ebb
Elin <c2973457@gmail.com>
2023-03-22 16:25:21 +0800

    no message

--- QQEgg_Backend/DTO/SuppliersDTO.cs
+++ QQEgg_Backend/DTO/SuppliersDTO.cs
@@ -1,10 +1,18 @@
 ﻿using QQEgg_Backend.Abstract;
+using System.Text.Json.Serialization;
 
 namespace QQEgg_Backend.DTO
 {
     public class SuppliersDTO: SupplierAbstractDTOValidation
     {
-        public int SupplierId { get; set; }
-       
-    }
+        public string Password { get; set; }
+		[JsonIgnore]
+		public string? PasswordHash { get; set; } // 新增一個屬性用來表示加密後的密碼
+
+		public void EncryptPassword()
+		{
+			PasswordHash = BCrypt.Net.BCryptHlper.HashPassword(Password);// 在註冊時將密碼加密後存入 EncryptedPassword 屬性中
+		}
+
+	}
 }
